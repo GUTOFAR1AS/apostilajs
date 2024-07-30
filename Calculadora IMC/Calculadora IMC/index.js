d164aab9b419d66dac474c2dd9a81096fbf8dc9a
Gustavo <122311037+GUTOFAR1AS@users.noreply.github.com>
2024-07-30 04:10:17 -0300

    Add files via upload

---
+++ Calculadora IMC/Calculadora IMC/index.js
@@ -0,0 +1,45 @@
+/*
+E nosso calculo devemos retornar os seguintes valores para o usuário:
+
+Resultado do Calc  | Resultado para o usuario 
+Abaixo de 17 | Muito abaixo do peso 
+Entre 17 e 18.49 | Abaixo do peso 
+Entre 18.5 e 24.99 | Peso normal
+Entre 25 e 29.99 | Acima de peso
+Entre 30 e 34.99 | Obesidade I
+Entre 35 e 39.99 | Obesidade II
+*/
+
+
+
+const peso = 90;
+const altura = 1.60;
+const imc = Number((peso / (altura * altura)).toFixed(2));
+
+
+console.log(imc);
+switch (true) {
+    case imc < 17: 
+        console.log("Muito abaixo do peso");
+        break;
+
+    case imc >= 17 && imc <= 18.49:
+        console.log ("Abaixo do peso")
+        break;
+
+    case imc >= 18.5 && imc <= 24.99:
+            console.log ("Peso Normal")
+            break;
+
+    case imc >= 25 && imc <= 29.99:
+                console.log ("Acima do peso")
+                break;
+
+    case imc >= 30 && imc <= 34.99:
+                console.log ("Obesidade I")
+                break;
+                
+    case imc >= 35 && imc <= 39.99:
+                console.log ("Obesidade II")
+                break;
+}  
